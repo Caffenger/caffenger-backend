7ea539e646d6f05efa06505eed075bc6d07ffcc5
Marko Mihajlovic <mih.mare@gmail.com>
2026-01-10 19:04:36 +0100

    - Added empty mock to avoid prisma error during e2e test

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginService } from './login/login.service';
 import { RegisterService } from './register/register.service';
-import { PrismaModule } from 'src/lib/prisma/prisma.module';
+import { PrismaModule } from '@/lib/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
