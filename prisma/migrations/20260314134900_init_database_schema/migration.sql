174f3df4c90f0552b1db3e433cc5de177bb10c44
Dw1GHTY <mih.mare@gmail.com>
2026-03-14 14:49:41 +0100

    - Project environments setup - Better application bootstrap logging - Reset migration history to fix git tracking issues and push a final database initialization migration

--- prisma/migrations/20260314134900_init_database_schema/migration.sql
+++ prisma/migrations/20260314134900_init_database_schema/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- CreateEnum
 CREATE TYPE "CafeStaffRole" AS ENUM ('OWNER', 'SERVER', 'BARISTA');
 
@@ -13,12 +7,17 @@ CREATE TYPE "ObjectTypeEnum" AS ENUM ('TABLE', 'SEAT', 'WALL');
 -- CreateEnum
 CREATE TYPE "MenuItemType" AS ENUM ('FOOD', 'DRINK');
 
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "name" TEXT NOT NULL,
-ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
 
--- DropIndex
-DROP INDEX "User_id_key";
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Cafe" (
@@ -127,6 +126,9 @@ CREATE TABLE "Bill" (
 
     CONSTRAINT "Bill_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Cafe" ADD CONSTRAINT "Cafe_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
